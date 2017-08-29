9370fd01b904ad537fe42a32611e54558bb28152
Holomorfo <holomorfo@users.noreply.github.com>
2017-08-29 12:38:41 -0700

    2

--- Class3-QWERTY-piano/qwerty.js
+++ Class3-QWERTY-piano/qwerty.js
@@ -8,6 +8,7 @@ var modMinDepth = -150;
 
 function setup(){
     console.log("Setup");
+    console.log("prueba cambios");
     // Carrier
     osc = new p5.Oscillator('sine');
     osc.amp(0); // set amplitude
